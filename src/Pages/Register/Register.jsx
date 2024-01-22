cf4eb88c52b239a4265d74626845c323cbd9063d
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2024-01-22 20:24:07 +0600

    navbar created

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,107 @@
+
+
+
+const Register = () => {
+    return (
+        <div>
+             {/* <div className="hero min-h-screen">
+        <div className="hero-content flex-col">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <h1 className="text-3xl font-bold mb-4">Register now!</h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name", { required: true })}
+                  name="name"
+                  placeholder="name"
+                  className="input input-bordered"
+                />
+                {errors.name && (
+                  <span className="text-red-600">This field is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">PhotoURL</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("photoURL", { required: true })}
+                  placeholder="photo url"
+                  className="input input-bordered"
+                />
+                {errors.photoURL && (
+                  <span className="text-red-600">This field is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+                {errors.email && (
+                  <span className="text-red-600">Email field is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  {...register("password", { required: true, minLength: 6 })}
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                {errors.password?.type === "required" && (
+                  <span className="text-red-600">
+                    Password field is required
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="text-red-600">
+                    Password must be 6 characters
+                  </span>
+                )}
+
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Register"
+                />
+              </div>
+            </form>
+            <p className="text-center">
+              <small>
+                Have an account? Please{" "}
+                <Link className="text-blue-700 text-2xl font-bold" to="/login">
+                  Login
+                </Link>
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>  */}
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+
+    const navLinks = <>
+       <Link to="/"><li className="lg:text-2xl font-bold">Home</li></Link>
+    </>
+    return (
+        <div>
+           <div className="navbar bg-gradient-to-r from-cyan-300 to bg-indigo-700 ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-1 shadow bg-base-100 rounded-box w-52">
+      
+      </ul>
+    </div>
+    <img className="w-16 h-16" src="https://i.ibb.co/RD1SJSJ/download-removebg-preview-1.png" alt="" />
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      {navLinks}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <button className="btn bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">Login</button>
+  </div>
+</div> 
+</div>
+    );
+};
+
+export default Navbar;
